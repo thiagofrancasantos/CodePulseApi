1e4fc53c26db18f04be9102bd5787eafdf99b920
thiagofrancasantos <francasantos.t@gmail.com>
2025-07-01 10:48:57 -0400

    repository done

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,88 @@
+using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+  private readonly ICategoryRepository categoryRepository;
+
+  public CategoriesController(ICategoryRepository categoryRepository)
+  {
+    this.categoryRepository = categoryRepository;
+  }
+
+  //
+  [HttpPost]
+  public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryRequestDto request)
+  {
+    // Map DTO to Domain Model
+
+    var category = new Category
+    {
+      Name = request.Name,
+      UrlHandle = request.UrlHandle
+    };
+
+    await categoryRepository.CreateAsync(category);
+
+
+    //Domain Model to DTo
+    var response = new CategoryDto
+    {
+      Id = category.Id,
+      Name = category.Name,
+      UrlHandle = category.UrlHandle
+    };
+
+    return Ok(response);
+
+  }
+
+    // GET: http://localhost:5261/api/Categories
+    [HttpGet]
+  public async Task<IActionResult> GetAllCategory(){
+    var categories = await categoryRepository.GetAllAsync();
+
+        // Map doamain model to DTO
+
+    var response = new List<CategoryDto>();
+    foreach (var category in categories) {
+            response.Add(new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            });  
+    }
+
+    return Ok(response);
+        
+
+  }
+
+    //GET: http://localhost:5261/api/categories/{id}
+    [HttpGet]
+    [Route("{id:Guid}")]
+    public async Task<IActionResult> GetCategoryById([FromRoute]Guid id)    {
+        var existingCategory = await categoryRepository.GetById(id);
+
+        if (existingCategory is null) {
+            return NotFound();
+        }
+
+        var response = new CategoryDto
+        {
+            Id = existingCategory.Id,
+            Name = existingCategory.Name,
+            UrlHandle = existingCategory.UrlHandle
+        };
+
+        return Ok(response);
+    }
+}
