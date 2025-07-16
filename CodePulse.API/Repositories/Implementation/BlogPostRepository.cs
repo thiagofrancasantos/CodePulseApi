249e3241efd1c82c50417a33b6104703d39f5bb3
thiagofrancasantos <francasantos.t@gmail.com>
2025-07-16 14:38:59 -0400

    delete blogpost funcionando no front e no back, implementado no repository e controller

--- CodePulse.API/Repositories/Implementation/BlogPostRepository.cs
+++ CodePulse.API/Repositories/Implementation/BlogPostRepository.cs
@@ -50,4 +50,17 @@ public class BlogPostRepository : IBlogPostRepository
 
         return blogPost;
     }
+
+    public async Task<BlogPost?> DeleteAsync(Guid id)
+    {
+        var existingBlogPost = await dbContext.BlogPosts.FirstOrDefaultAsync(x => x.Id == id);
+        if(existingBlogPost != null)
+        {
+            dbContext.BlogPosts.Remove(existingBlogPost);
+            await dbContext.SaveChangesAsync();
+            return existingBlogPost;
+        }
+
+        return null;
+    }
 }
--- CodePulse.API/Repositories/Interface/IBlogPostRepository.cs
+++ CodePulse.API/Repositories/Interface/IBlogPostRepository.cs
@@ -6,8 +6,7 @@ public interface IBlogPostRepository
 {
     Task<BlogPost> CreateAsync(BlogPost blogPost);
     Task<IEnumerable<BlogPost>> GetAllAsync();
-
     Task<BlogPost?> GetByIdAsync(Guid id);
-
     Task<BlogPost?> UpdateAsync(BlogPost blogPost);
+    Task<BlogPost?> DeleteAsync(Guid id);
 }
