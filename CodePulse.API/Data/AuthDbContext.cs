20f07f2d8e58bd1bc513387cda6a7f6692e710af
thiagofrancasantos <francasantos.t@gmail.com>
2025-07-29 14:40:52 -0400

    AuthDbContext adicionado e configurado no Program.cs

---
+++ CodePulse.API/Data/AuthDbContext.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data;
+
+public class AuthDbContext : IdentityDbContext
+{
+    public AuthDbContext(DbContextOptions options) : base(options)
+    {    
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        var readerRoleId = "c6bc7e7e-01a4-4a27-8271-4b0aa6767ba5";
+        var writerRoleId = "dbe07dcc-bdcd-45bc-81dc-56fdf52559e6";
+
+        // Create reader and writer role
+        var roles = new List<IdentityRole>
+        {
+            new IdentityRole()
+            {
+                Id = readerRoleId,
+                Name = "Reader",
+                NormalizedName = "Reader".ToUpper(),
+                ConcurrencyStamp = readerRoleId
+            },
+
+            new IdentityRole(){
+                Id = writerRoleId,
+                Name = "Writer",
+                NormalizedName = "Writer".ToUpper(),
+                ConcurrencyStamp= writerRoleId
+            }
+        };
+
+        // Seed the roles
+        builder.Entity<IdentityRole>().HasData(roles);
+
+        // Create an Admin User
+        var adminUserId = "c9a183af-f94f-41c9-9be4-970a6e69539b";
+        var admin = new IdentityUser()
+        {
+            Id = adminUserId,
+            UserName = "admin@codepulse.com",
+            Email = "admin@codepulse.com",
+            NormalizedEmail = "admin@codepulse.com".ToUpper(),
+            NormalizedUserName = "admin@codepulse.com".ToUpper()
+        };
+
+        admin.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(admin, "Admin@123");
+
+        builder.Entity<IdentityUser>().HasData(admin);
+
+        // Give Roles to Admin
+        var adminRoles = new List<IdentityUserRole<string>>()
+        {
+            new()
+            {
+               UserId = adminUserId,
+               RoleId = readerRoleId
+            },
+            new()
+            {
+                UserId = adminUserId,
+                RoleId = writerRoleId
+            }
+        };
+
+        builder.Entity<IdentityUserRole<string>>().HasData(adminRoles);
+    }
+}
--- CodePulse.API/Program.cs
+++ CodePulse.API/Program.cs
@@ -1,6 +1,9 @@
 using CodePulse.API.Data;
 using CodePulse.API.Repositories.Implementation;
 using CodePulse.API.Repositories.Interface;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using System;
@@ -19,7 +22,42 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
 builder.Services.AddScoped<IImageRepository, ImageRepository>();
 
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider < DataProtectorTokenProvider<IdentityUser>>("Codepulse")
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            AuthenticationType = "Jwt",
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
@@ -38,6 +76,7 @@ app.UseCors(options =>
     options.AllowAnyOrigin();
 });
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseStaticFiles(new StaticFileOptions
